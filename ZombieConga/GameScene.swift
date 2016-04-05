db5e80cff7ec38f04d22e7465b323d9f6f91135b
AnkoTop <anko.top@reactivity.nl>
2016-04-05 21:37:23 +0200

    Chapter 3 part 1

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -18,6 +18,8 @@ class GameScene: SKScene {
     let zombieRotateRadiansPerSec:CGFloat = 4.0 * π
     var velocity = CGPoint.zero
     let playableRect: CGRect
+    let zombieAnimation: SKAction
+    
     
     var lastTouchedLocation : CGPoint?
     
@@ -27,6 +29,20 @@ class GameScene: SKScene {
         let playableHeight = size.width / maxAspectRatio // 2
         let playableMargin = (size.height-playableHeight)/2.0 // 3
         playableRect = CGRect(x: 0, y: playableMargin, width: size.width, height: playableHeight) // 4
+        
+        //animate the zombie
+        // 1
+        var textures:[SKTexture] = []
+        // 2
+        for i in 1...4 {
+            textures.append(SKTexture(imageNamed: "zombie\(i)"))
+        }
+        // 3
+        textures.append(textures[2])
+        textures.append(textures[1])
+        // 4
+        zombieAnimation = SKAction.animateWithTextures(textures, timePerFrame: 0.1)
+        
         super.init(size: size) // 5
     }
     
@@ -48,6 +64,17 @@ class GameScene: SKScene {
         // #2: The Zombie
         zombie.position = CGPoint(x: 400, y: 400)
         addChild(zombie)
+        // animate the walk
+        //zombie.runAction(SKAction.repeatActionForever(zombieAnimation))
+        
+        // #3a: The Enemy (once)
+        //spawnEnemyOnce()
+        // #3b: The Enemy (spawning)
+        runAction(SKAction.repeatActionForever(SKAction.sequence([SKAction.runBlock(spawnEnemy),SKAction.waitForDuration(2.0)])))
+        
+        // #4: The Cats
+        runAction(SKAction.repeatActionForever(SKAction.sequence([SKAction.runBlock(spawnCat),SKAction.waitForDuration(1.0)])))
+        
         
         //@DEBUG
         //debugDrawPlayableArea()
@@ -67,6 +94,8 @@ class GameScene: SKScene {
         if lastTouchedLocation != nil && (lastTouchedLocation! - zombie.position).length() <= amountToMoveZombie {
             zombie.position = lastTouchedLocation!
             velocity = CGPoint(x: 0, y: 0)
+            // stop the walk animation
+            stopZombieAnimation()  
         } else {
             moveSprite(zombie, velocity: velocity)
             // make sure the zombie stays on the screen
@@ -98,12 +127,29 @@ class GameScene: SKScene {
     
     
     func moveZombieToward(location: CGPoint) {
+        // start the walk animation
+        startZombieAnimation()
         // get the offset
         let offset = location - zombie.position
         let direction = offset.normalized()
         velocity = direction * zombieMovePointsPerSec
         
     }
+    
+    func startZombieAnimation() {
+    
+        if zombie.actionForKey("animation") == nil {
+            zombie.runAction(
+                SKAction.repeatActionForever(zombieAnimation),
+                withKey: "animation")
+        }
+    }
+    
+    func stopZombieAnimation() {
+    
+        zombie.removeActionForKey("animation")
+    }
+    
     
     func boundsCheckZombie() {
  
@@ -127,6 +173,100 @@ class GameScene: SKScene {
             velocity.y = -velocity.y
         }
     }
+    
+    func spawnEnemyOnce() {
+        //Create the enemy
+        let enemy = SKSpriteNode(imageNamed: "enemy")
+        enemy.position = CGPoint(x: size.width + enemy.size.width/2, y: size.height/2)
+        addChild(enemy)
+        //And make it move in a straight line
+        //let actionMove = SKAction.moveTo(CGPoint(x: -enemy.size.width/2, y: enemy.position.y), duration: 2.0)
+        //enemy.runAction(actionMove)
+        
+        // Or move in a V-shape
+        // 1 non reversible
+        //let actionMidMove = SKAction.moveTo(CGPoint(x: size.width/2, y: CGRectGetMinY(playableRect) + enemy.size.height/2), duration: 1.0)
+        // 2 non reversible
+        //let actionMove = SKAction.moveTo(CGPoint(x: -enemy.size.width/2, y: enemy.position.y), duration:1.0)
+        // 1 reversible
+        let actionMidMove = SKAction.moveByX(-size.width/2-enemy.size.width/2, y: -CGRectGetHeight(playableRect)/2 + enemy.size.height/2, duration: 1.0)
+        // 2 reversible
+        let actionMove = SKAction.moveByX(-size.width/2-enemy.size.width/2, y: CGRectGetHeight(playableRect)/2 - enemy.size.height/2, duration: 1.0)
+        
+        
+        // 3a simple
+        //let sequence = SKAction.sequence([actionMidMove, actionMove])
+        // 3b with wait
+        //let wait = SKAction.waitForDuration(0.25)
+        //let sequence = SKAction.sequence([actionMidMove, wait, actionMove])
+        //3c with wait + log message in block
+        let wait = SKAction.waitForDuration(0.25)
+        let logMessage = SKAction.runBlock() {
+            print("Reached bottom!")
+            // could be any code here...
+        }
+        // non reversible
+        //let sequence = SKAction.sequence([actionMidMove, logMessage, wait, actionMove])
+        //reversible
+        //let reverseMid = actionMidMove.reversedAction()
+        //let reverseMove = actionMove.reversedAction()
+        //let sequence = SKAction.sequence([actionMidMove, logMessage, wait, actionMove, reverseMove, logMessage, wait, reverseMid])
+        let halfSequence = SKAction.sequence([actionMidMove, logMessage, wait, actionMove])
+        let sequence = SKAction.sequence([halfSequence, halfSequence.reversedAction()])
+        // 4a single action
+        //enemy.runAction(sequence)
+        // 4b forever repeating action
+        let repeatAction = SKAction.repeatActionForever(sequence)
+        enemy.runAction(repeatAction)
+    }
+    
+    func spawnEnemy() {
+        //Create the enemy
+        let enemy = SKSpriteNode(imageNamed: "enemy")
+        enemy.position = CGPoint(x: size.width + enemy.size.width/2, y: CGFloat.random(min: CGRectGetMinY(playableRect) + enemy.size.height/2, max: CGRectGetMaxY(playableRect) - enemy.size.height/2))
+        addChild(enemy)
+        //And make it move in a straight line
+        let actionMove = SKAction.moveTo(CGPoint(x: -enemy.size.width/2, y: enemy.position.y), duration: 2.0)
+        //enemy.runAction(actionMove)
+        // make sure to remove it afterwards...
+        let actionRemove = SKAction.removeFromParent()
+        enemy.runAction(SKAction.sequence([actionMove, actionRemove]))
+    }
+    
+    
+    func spawnCat() {
+        // 1
+        let cat = SKSpriteNode(imageNamed: "cat")
+        cat.position = CGPoint(
+            x: CGFloat.random(min: CGRectGetMinX(playableRect),
+                max: CGRectGetMaxX(playableRect)),
+            y: CGFloat.random(min: CGRectGetMinY(playableRect),
+                max: CGRectGetMaxY(playableRect)))
+        cat.setScale(0)
+        addChild(cat)
+        // 2
+        let appear = SKAction.scaleTo(1.0, duration: 0.5)
+        // a) waittime before disappearing
+        //let wait = SKAction.waitForDuration(10.0)
+        // or wiggle while waiting
+        cat.zRotation = -π / 16.0
+        let leftWiggle = SKAction.rotateByAngle(π/8.0, duration: 0.5)
+        let rightWiggle = leftWiggle.reversedAction()
+        let fullWiggle = SKAction.sequence([leftWiggle, rightWiggle])
+        let wiggleWait = SKAction.repeatAction(fullWiggle, count: 10)
+        
+        let disappear = SKAction.scaleTo(0, duration: 0.5)
+        let removeFromParent = SKAction.removeFromParent()
+        // a) the just wait type
+        //let actions = [appear, wait, disappear, removeFromParent]
+        // b) the wiggle type
+        let actions = [appear, wiggleWait, disappear, removeFromParent]
+        
+        cat.runAction(SKAction.sequence(actions))
+    }
+    
+    
+    
     
     func sceneTouched(touchLocation:CGPoint) {
         lastTouchedLocation = touchLocation
